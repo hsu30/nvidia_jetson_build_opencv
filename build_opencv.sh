cd09c8536ae60284c2de7daff28f62922f699f93
hsu30 <59867659+hsu30@users.noreply.github.com>
2023-10-11 00:02:31 +0800

    Update build_opencv.sh

--- build_opencv.sh
+++ build_opencv.sh
@@ -110,7 +110,6 @@ configure () {
         -D CUDA_ARCH_BIN=5.3,6.2,7.2,8.7
         -D CUDA_ARCH_PTX=
         -D CUDA_FAST_MATH=ON
-        -D CUDNN_VERSION='8.0'
         -D EIGEN_INCLUDE_PATH=/usr/include/eigen3 
         -D ENABLE_NEON=ON
         -D OPENCV_DNN_CUDA=ON
